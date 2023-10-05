df8558b33a962668b8bd091d1b1570cd68f9f205
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-10-06 01:07:34 +0530

    Second Commit

--- JavaPrograms/src/main/Identifiers.java
+++ JavaPrograms/src/main/Identifiers.java
@@ -2,9 +2,11 @@ package main;
 
 public class Identifiers {
 
-	public static void main(String[] args) {
+	public static void main(java.lang.String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("jalajagarwal");
+		System.out.println("Agarwal");
+		
 
 	}
 
