6cccafbf271f254e2a5484962b3af247a30c8bee
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-10-17 12:46:19 +0530

    Second Commit

--- JavaPrograms/src/main/CountObject.java
+++ JavaPrograms/src/main/CountObject.java
@@ -3,6 +3,7 @@ import java.lang.String;
 class App
 {
 	static int price;
+	//int price then error.
 	static int count;
 	{
 		count++;
@@ -21,7 +22,7 @@ public class CountObject {
 		App  nokia =new  App(2000);
 		App  iphone=new  App (200000);
 		System.out.println(App.count);
-		System.out.println(nokia.price);
+		System.out.println(nokia.price);// static variable caan be accessed everywhere but instance cannot be accessed inside static method static block. instance cannot be accessed directly inside main method .
 		
 		
 		
