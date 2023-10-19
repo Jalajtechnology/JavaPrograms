52cc026633ed76c1bf1e02c9c11576acecd847bf
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-10-19 11:15:40 +0530

    First

---
+++ JavaPrograms/src/main/SingleInheritence.java
@@ -0,0 +1,25 @@
+package main;
+import java.lang.String;
+class Parent
+{
+	void display()
+	{
+		System.out.println("Welcome to our program");
+	}
+}
+class Child extends Parent
+{
+	
+}
+
+public class SingleInheritence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child  r=new Child();
+		r.display();
+		
+
+	}
+
+}
