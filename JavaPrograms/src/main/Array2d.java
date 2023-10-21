071fec999f32eb05310a0b9e51629fc08d77dc4e
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-10-21 16:35:41 +0530

    First

---
+++ JavaPrograms/src/main/Array2d.java
@@ -0,0 +1,15 @@
+package main;
+import java.lang.String;
+
+public class Array2d {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] a=new int [2][3];
+		System.out.println(a);
+		System.out.println(a[0]);
+		System.out.println(a[0][0]);
+
+	}
+
+}
