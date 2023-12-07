61ed181197d8e9b0534284d38e049a6c33a1fb45
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-12-08 00:00:23 +0530

    First

---
+++ JavaPrograms/src/Interface/Relationship.java
@@ -0,0 +1,22 @@
+package Interface;
+interface IOne
+{
+	void methodOne();
+}
+interface ITwo
+{
+	void methodTwo();
+}
+interface IThree extends IOne,ITwo
+{
+	
+}
+
+public class Relationship {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
