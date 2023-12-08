d7cd5eaa01b1b06bd1c0e5f3f9f5fdba7b0a6b0f
Jalajtechnology <jalajagarwal1234@gmail.com>
2023-12-08 18:28:15 +0530

    First

---
+++ JavaPrograms/src/main/Program.java
@@ -0,0 +1,13 @@
+package main;
+import java.lang.String;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str=null;
+		System.out.println(str);
+
+	}
+
+}
