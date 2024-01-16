3efab5ac78afa73f9a6f358633e369af84d0dcf5
Jalajtechnology <jalajagarwal1234@gmail.com>
2024-01-16 18:45:53 +0530

    Committed

---
+++ JavaPrograms/src/Wrapper/parseMethod.java
@@ -0,0 +1,12 @@
+package Wrapper;
+
+public class parseMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i1=Integer.parseInt("10");
+		System.out.println(i1);
+
+	}
+
+}
