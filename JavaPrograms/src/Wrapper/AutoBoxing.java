7fb16796a32ac6f69183793fb726efb967937ec2
Jalajtechnology <jalajagarwal1234@gmail.com>
2024-01-18 16:01:03 +0530

    First

--- JavaPrograms/src/Wrapper/AutoBoxing.java
+++ JavaPrograms/src/Wrapper/AutoBoxing.java
@@ -4,12 +4,12 @@ public class AutoBoxing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Integer i1=100;
-		Integer i2=100;
-		System.out.println(i1==i2);
-		Integer i3=1000;
-		Integer i4=1000;
-		System.out.println(i3==i4);
+		Integer a=100;
+		Integer b=100;
+		System.out.println(a==b);
+		Integer i=1000;
+		Integer j=1000;
+		System.out.println(i==j);
 
 	}
 
