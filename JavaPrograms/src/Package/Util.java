d298f31eef5909cc2c8c6a37ea0beb9042b7568b
Jalajtechnology <jalajagarwal1234@gmail.com>
2024-04-12 18:03:51 +0530

    Second

---
+++ JavaPrograms/src/Package/Util.java
@@ -0,0 +1,13 @@
+package Package;
+import java.util.Date;
+import java.sql.Date;
+
+public class Util {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d=new Date();
+
+	}
+
+}
